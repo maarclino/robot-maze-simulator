b073cebd274192fdf203cb6d6c8fd4dd02ddc0d2
Marc Lino <marclinoa3@gmail.com>
2026-07-22 23:30:37 +0200

    Update FinchMove.java

--- FinchMove.java
+++ FinchMove.java
@@ -1,14 +1,3 @@
-/*
- * FinchMove.java        1.0 Feb 1, 2022
- *
- * Models the program.
- *
- * Copyright 2022 Rafel Botey Agusti <rbotey@escoladeltreball.org>
- *
- * This is free software, licensed under the GNU General Public License v3.
- * See http://www.gnu.org/licenses/gpl.html for more information.
- */
-
 package part2;
 import finchRobot.Finch;
 import part2.FinchAction;
